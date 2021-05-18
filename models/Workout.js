779ebb2828c2e1a4880bfdc5f738f9e39ff84d7f
Michael Francisco <m.francisco9@gmail.com>
2021-05-18 16:05:26 -0500

    added models

---
+++ models/Workout.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Scheme = mongoose.Schema;
+
+const exerciseSchema = new mongoose.Schema({
+    type: {
+
+    },
+    name: {
+
+    },
+    duration: {
+
+    },
+    weight: {
+
+    },
+    sets: {
+
+    },
+    reps: {
+
+    },
+    distance: {
+
+    }
+});
