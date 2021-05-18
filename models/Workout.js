b74ea35ed3e9e3dcfa683611bd977cf7de3e842b
Michael Francisco <m.francisco9@gmail.com>
2021-05-18 16:14:01 -0500

    index in models

--- models/Workout.js
+++ models/Workout.js
@@ -3,24 +3,32 @@ const Scheme = mongoose.Schema;
 
 const exerciseSchema = new mongoose.Schema({
     type: {
+        type: String,
+        require: `Please choose either 'resistance' or 'cardio'.`
 
     },
     name: {
+        type: String,
 
     },
     duration: {
+        type: Number,
 
     },
     weight: {
+        type: Number,
 
     },
     sets: {
+        type: Number,
 
     },
     reps: {
+        type: Number,
 
     },
     distance: {
+        type: Number,
 
     }
 });
