8bc4e174dee33c437a867a49eb5082e6018c6ce3
Michael Francisco <m.francisco9@gmail.com>
2021-05-19 09:39:23 -0500

    routes should be complete

--- routes/html/index.js
+++ routes/html/index.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
 const path = require('path');
 
+//homepage
+
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname + "../index.html"));
   });
   
+//stats page
+
   router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname + "/../../public/stats.html"));
   });
   
+
+//exercise submit page
+
   router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname + "/../../public/exercise.html"));
   });
