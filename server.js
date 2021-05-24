03500c2c6aac1550060263ec4fe4f5948e3fe6ea
Michael Francisco <m.francisco9@gmail.com>
2021-05-23 21:08:19 -0500

    changed routes subfolders, renamed js files

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+//setting port to 3000
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
+//using logger in dev
 app.use(logger('dev'));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//establishing static folder
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -18,8 +22,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 // routes
-app.use(require("./routes/html/index.js"));
-// app.use(require("./routes/api/index.js"));
+app.use(require("./routes/html-routes.js"));
+app.use(require("./routes/api-routes.js"));
 
 
 // Start the server
